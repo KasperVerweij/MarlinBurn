cb20ee28c8829c3db6246610ae0babc8260d7665
KasperVerweij <49719455+KasperVerweij@users.noreply.github.com>
2023-04-26 08:42:22 +0200

    Add project files.

---
+++ MarlinBurn/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace MarlinBurn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(53, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(765, 451);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.comboBox3);
+            this.panel3.Controls.Add(this.Connect);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.comboBox2);
+            this.panel3.Location = new System.Drawing.Point(114, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(443, 28);
+            this.panel3.TabIndex = 7;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(200, 4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(99, 21);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(352, 2);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 7;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PORT";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(65, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(99, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DtrEnable = true;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(590, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Barf Marlin";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 591);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 215);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 835);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MarlinBurn/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace MarlinBurn
+{
+    public partial class Form1 : Form
+    {
+        int[] baudrates = { 110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200, 128000, 256000 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap Picture = new Bitmap(10, 10);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog browser = new OpenFileDialog
+            {
+                ///DefaultExt = "BMP",
+                InitialDirectory = "C:\\",
+                //Filter = "RoboCell Sleeve Excel List (*.xlsx)|*.XLSX"
+            };
+            DialogResult resultpath = browser.ShowDialog();
+            if (resultpath == DialogResult.OK)
+            {
+
+                Picture = (Bitmap)Image.FromFile(browser.FileName);
+                pictureBox1.Image = Picture;
+            }
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+
+            if (serialPort1 == null) serialPort1 = new SerialPort();
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                Connect.Text = "Connect";
+
+
+            }
+            else
+            {
+                serialPort1.BaudRate = (int)comboBox3.SelectedItem;
+                serialPort1.PortName = comboBox2.Text;
+                serialPort1.Open();
+                Connect.Text = "Disconnect";
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                comboBox2.Items.Add(port);
+            }
+            foreach (int i in baudrates)
+            {
+                comboBox3.Items.Add(i);
+            }
+            comboBox3.SelectedItem = baudrates[11];
+            comboBox2.SelectedIndex = 0;
+        }
+        bool playing = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            playing = true;
+            cansend = true;
+        }
+        string[] gcodemeuk = { "G90", "M3 S0","G0 X0 Y0 S0", "G0 X10 Y10 S0", "G0 X0 Y0 S10", "G0 X10 Y10 S10" };
+        int index = 0;
+        bool cansend = true;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (playing)
+            {
+
+                if (cansend)
+                {
+                    serialPort1.Write(gcodemeuk[index] + "\n");
+                    textBox1.Text += gcodemeuk[index]+ "\n";  
+                    index++;
+                    cansend = false;
+                }
+
+               
+                if(index >= gcodemeuk.Length)
+                { playing = false;
+                    index = 0;
+                }
+            }        
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // is ok signal
+            cansend = true;
+           // if (serialPort1.BytesToRead > 1)
+             //   if (serialPort1.ReadLine().Contains("ok"))
+                   
+            //else
+               
+
+        }
+    }
+}
