de101707a386db6a466bba7554f98a7cbcfb22d7
KasperVerweij <49719455+KasperVerweij@users.noreply.github.com>
2023-05-06 22:57:36 +0200

    almost working

--- MarlinBurn/Form1.Designer.cs
+++ MarlinBurn/Form1.Designer.cs
@@ -40,6 +40,21 @@
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.XSizeTb = new System.Windows.Forms.TextBox();
+            this.YSixeTb = new System.Windows.Forms.TextBox();
+            this.DpiTb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.NozzOffsY = new System.Windows.Forms.TextBox();
+            this.NozzleOffsX = new System.Windows.Forms.TextBox();
+            this.HomeZCheckBox = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.MaterialThicknessTb = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
@@ -56,9 +71,9 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(53, 75);
+            this.pictureBox1.Location = new System.Drawing.Point(376, 134);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(765, 451);
+            this.pictureBox1.Size = new System.Drawing.Size(495, 451);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -118,7 +133,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(590, 16);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(201, 23);
+            this.button2.Size = new System.Drawing.Size(121, 23);
             this.button2.TabIndex = 9;
             this.button2.Text = "Barf Marlin";
             this.button2.UseVisualStyleBackColor = true;
@@ -132,17 +147,166 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(286, 591);
+            this.textBox1.Location = new System.Drawing.Point(12, 134);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(357, 215);
+            this.textBox1.Size = new System.Drawing.Size(357, 689);
             this.textBox1.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(739, 800);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Export Gcode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // XSizeTb
+            // 
+            this.XSizeTb.Location = new System.Drawing.Point(179, 49);
+            this.XSizeTb.Name = "XSizeTb";
+            this.XSizeTb.Size = new System.Drawing.Size(100, 20);
+            this.XSizeTb.TabIndex = 12;
+            this.XSizeTb.Text = "100";
+            // 
+            // YSixeTb
+            // 
+            this.YSixeTb.Location = new System.Drawing.Point(179, 75);
+            this.YSixeTb.Name = "YSixeTb";
+            this.YSixeTb.Size = new System.Drawing.Size(100, 20);
+            this.YSixeTb.TabIndex = 13;
+            this.YSixeTb.Text = "100";
+            // 
+            // DpiTb
+            // 
+            this.DpiTb.Location = new System.Drawing.Point(179, 101);
+            this.DpiTb.Name = "DpiTb";
+            this.DpiTb.Size = new System.Drawing.Size(100, 20);
+            this.DpiTb.TabIndex = 14;
+            this.DpiTb.Text = "12";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Width[mm]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Height[mm]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "DPI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(302, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "NozzOffsY";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(304, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "NozzOffsX";
+            // 
+            // NozzOffsY
+            // 
+            this.NozzOffsY.Location = new System.Drawing.Point(367, 75);
+            this.NozzOffsY.Name = "NozzOffsY";
+            this.NozzOffsY.Size = new System.Drawing.Size(100, 20);
+            this.NozzOffsY.TabIndex = 19;
+            this.NozzOffsY.Text = "24";
+            this.NozzOffsY.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // NozzleOffsX
+            // 
+            this.NozzleOffsX.Location = new System.Drawing.Point(367, 49);
+            this.NozzleOffsX.Name = "NozzleOffsX";
+            this.NozzleOffsX.Size = new System.Drawing.Size(100, 20);
+            this.NozzleOffsX.TabIndex = 18;
+            this.NozzleOffsX.Text = "0";
+            // 
+            // HomeZCheckBox
+            // 
+            this.HomeZCheckBox.AutoSize = true;
+            this.HomeZCheckBox.Checked = true;
+            this.HomeZCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.HomeZCheckBox.Location = new System.Drawing.Point(554, 51);
+            this.HomeZCheckBox.Name = "HomeZCheckBox";
+            this.HomeZCheckBox.Size = new System.Drawing.Size(64, 17);
+            this.HomeZCheckBox.TabIndex = 22;
+            this.HomeZCheckBox.Text = "Home Z";
+            this.HomeZCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(628, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Material Thickness";
+            // 
+            // MaterialThicknessTb
+            // 
+            this.MaterialThicknessTb.Location = new System.Drawing.Point(631, 68);
+            this.MaterialThicknessTb.Name = "MaterialThicknessTb";
+            this.MaterialThicknessTb.Size = new System.Drawing.Size(100, 20);
+            this.MaterialThicknessTb.TabIndex = 23;
+            this.MaterialThicknessTb.Text = "20";
+            this.MaterialThicknessTb.TextChanged += new System.EventHandler(this.MaterialThicknessTb_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(376, 608);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(357, 215);
+            this.textBox2.TabIndex = 25;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(872, 835);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.MaterialThicknessTb);
+            this.Controls.Add(this.HomeZCheckBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.NozzOffsY);
+            this.Controls.Add(this.NozzleOffsX);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DpiTb);
+            this.Controls.Add(this.YSixeTb);
+            this.Controls.Add(this.XSizeTb);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.panel3);
@@ -172,6 +336,21 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox XSizeTb;
+        private System.Windows.Forms.TextBox YSixeTb;
+        private System.Windows.Forms.TextBox DpiTb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox NozzOffsY;
+        private System.Windows.Forms.TextBox NozzleOffsX;
+        private System.Windows.Forms.CheckBox HomeZCheckBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox MaterialThicknessTb;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- MarlinBurn/Form1.cs
+++ MarlinBurn/Form1.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+using System.IO.IsolatedStorage;
+using System.Data.SqlTypes;
 
 namespace MarlinBurn
 {
@@ -22,6 +24,10 @@ namespace MarlinBurn
             InitializeComponent();
         }
         Bitmap Picture = new Bitmap(10, 10);
+
+        double NozzleXOffset = 0;
+        double NozzleYOffset = 0;
+
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog browser = new OpenFileDialog
@@ -93,15 +99,17 @@ namespace MarlinBurn
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            textBox2.Text = zwaffel;
             if (playing)
             {
 
                 if (cansend)
                 {
+                    cansend = false;
                     serialPort1.Write(gcodemeuk[index] + "\n");
                     textBox1.Text += gcodemeuk[index]+ "\n";  
                     index++;
-                    cansend = false;
+                    
                 }
 
                
@@ -111,16 +119,233 @@ namespace MarlinBurn
                 }
             }        
         }
+        string zwaffel = "hello\r\n";
 
         private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             // is ok signal
-            cansend = true;
-           // if (serialPort1.BytesToRead > 1)
-             //   if (serialPort1.ReadLine().Contains("ok"))
-                   
+            
+
+            string ser = serialPort1.ReadLine();
+            zwaffel += ser; 
+            if(ser.Contains("ok"))
+            {
+                cansend = true;
+
+            }
+
             //else
+
+
+        }
+        string GCodeLinMove(double x, double y, int power = 0)
+        {
+            x += NozzleXOffset;
+            y += NozzleYOffset;
+
+            x = System.Math.Round(x, 3);
+               y = System.Math.Round(y, 3);
+
+            return GCode("G1 X"+ GCodeFloat(x.ToString())  +" Y" + GCodeFloat(y.ToString()) + " S" + power);
+        }
+       string GCodeFloat(string inp)
+        {
+            return inp.Replace(',', '.');
+
+
+        }
+        string GCode(string inp)
+        {
+            return inp + "\r\n";
+        }
+        Bitmap outputBMP = new Bitmap(10, 10);
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // export gcode
+
+            double exportwidth = Double.Parse(XSizeTb.Text);
+            double exportheight = Double.Parse(YSixeTb.Text);
+            double pixeldense = Double.Parse(DpiTb.Text);
+            bool HomeZ = HomeZCheckBox.Checked;
+            double MaterialThickness = Double.Parse(MaterialThicknessTb.Text);
+            NozzleXOffset = Double.Parse(NozzleOffsX.Text);
+            NozzleYOffset = Double.Parse(NozzOffsY.Text);
+
+
+
+
+
+
+            string gcode = GCode("G28 X Y" + (HomeZ ? " Z" : ""));
+            gcode += GCode("G90"); // absolute positioning
+            if(HomeZ)
+            {
+                gcode += GCode("G0 Z" + GCodeFloat(MaterialThickness.ToString())); // Move up by how thick material is
+            }
+            gcode += GCode("M3I");
+
+            // initial square
+            gcode += GCodeLinMove(0, 0);
+            gcode += GCodeLinMove(exportwidth, 0);
+            gcode += GCodeLinMove(exportwidth, exportheight);
+            gcode += GCodeLinMove(0, exportheight);
+            gcode += GCodeLinMove(0, 0);
+
+            gcode += GCode("M3 S255");
+            // laser square
+            // test square
+            if (true)
+            {
+                gcode += GCodeLinMove(10, 0, 255);
+                gcode += GCodeLinMove(10, 10, 255);
+                gcode += GCodeLinMove(0, 10, 255);
+                gcode += GCodeLinMove(0, 0, 255);
+            }
+            // actual drawing gets done here
+
+
+            // first scale bitmap to right size
+            int xpixels =(int)( exportwidth * (pixeldense/25.4));
+            int ypixels = (int)(exportheight *( pixeldense/25.4));
+
+
+            outputBMP = new Bitmap(xpixels, ypixels);
+
+            using (System.Drawing.Graphics g = Graphics.FromImage(outputBMP)) 
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImage(Picture, 0, 0, xpixels, ypixels);
+
                
+
+
+            }
+            pictureBox1.Image = outputBMP;
+            pictureBox1.Update();
+            for (int y = 0; y < ypixels; y++)
+            {
+                for (int x = 0; x < xpixels; x++)
+                {
+                    // draw pixeloutputBMP.GetPixel(x, y).R
+                    Color c = outputBMP.GetPixel(x, y);
+                    int pow = 255 -( (c.R + c.G + c.B )/3);
+                    if (pow < 255)
+                    {
+                        gcode += GCodeLinMove((double)x / (pixeldense / 25.4), (double)y / (pixeldense / 25.4) ,255- pow);
+                    }
+
+                    else
+                    {
+                        gcode += GCodeLinMove((double)x / (pixeldense / 25.4), (double)y / (pixeldense / 25.4));
+                    }
+                }
+
+                // move back
+                gcode += GCodeLinMove(0, (double)y / (pixeldense / 25.4));
+            }
+
+
+
+
+
+
+
+
+
+
+            gcode += GCode("M3 S0"); // disable laser power
+                                     // gcode += GCode("")
+
+
+            renderGcode(gcode, exportwidth, exportheight, NozzleXOffset,NozzleYOffset);
+            
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\output.gcode";
+
+            gcodemeuk = gcode.Split('\n');
+            playing = true;
+            cansend = true;
+
+            File.WriteAllText(path,gcode);
+        }
+        const int bedrendersize = 5000;
+        private void renderGcode(string gcode, double bedsizex, double bedsizey, double xoffset, double yoffset)
+        {
+            string[] gcodes = gcode.Split('\n');
+
+            Bitmap printbed = new Bitmap(bedrendersize, bedrendersize);
+            double lastXpos = 0;
+            double lastYpos = 0;  
+            double scalarX = bedrendersize / bedsizex ;
+            double scalarY = bedrendersize / bedsizey;
+
+            Pen pen = new Pen(Color.Black);
+            pen.Width =  (int)scalarX/2;
+
+            
+
+
+
+
+
+            using (System.Drawing.Graphics g = Graphics.FromImage(printbed))
+            {
+               // g.FillRectangle(Brushes.White, 0,0,printbed.Size.Width,printbed.Size.Height);
+                foreach (string code in gcodes)
+                {
+                    if(code.Contains("G0") || code.Contains("G1"))
+                    {
+                        if (code.Contains('X') && code.Contains('Y'))
+                        {
+                            string xstring = code.Split('X')[1].Split(' ')[0];
+                            string ystring = code.Split('Y')[1].Split(' ')[0];
+                            xstring = xstring.Replace('.', ',').Replace("\r", "");
+                            ystring = ystring.Replace('.', ',').Replace("\r", "");
+                            double x = double.Parse(xstring) - NozzleXOffset;
+                            double y = double.Parse(ystring) - NozzleYOffset;
+
+                            //move command, draw this is s>0
+                            if (code.Contains('S'))
+                            {
+                                string sstring = code.Split('S')[1].Split(' ')[0];
+                                sstring = sstring.Replace('.', ',').Replace("\r","");
+                                
+                                double s = double.Parse(sstring);
+
+                                if (s > 0)
+                                {
+                                    pen.Color = Color.FromArgb((int)(255-s), (int)(255 - s), (int)(255 - s));
+                                                            //else
+                                                            //  pen.Color = Color.Firebrick;// Color.FromArgb((int)(255 - s), (int)(255 - s), (int)(255 - s));
+
+                                    g.DrawLine(pen, new Point((int)(lastXpos * scalarX), (int)(lastYpos * scalarY)), new Point((int)(x * scalarX), (int)(y * scalarY)));
+                                }
+                                }
+                            lastXpos = x;
+                            lastYpos = y;
+                        }
+                    }
+
+
+                }
+            }
+
+
+
+
+            pictureBox1.Image = printbed;
+            pictureBox1.Update();
+
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaterialThicknessTb_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
